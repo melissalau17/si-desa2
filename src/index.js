15fbe4eb684d8a8931e32e32c610b700ba74aeb9
melissalau17 <melissalau17300@gmail.com>
2025-09-02 22:15:41 +0700

    set file limit 50mb

--- src/index.js
+++ src/index.js
@@ -38,8 +38,8 @@ const io = new Server(server, {
 });
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
