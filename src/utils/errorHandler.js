8fba09d510077e29d5c938c388a5bea9bb4f38fd
melissalau17 <melissalau17300@gmail.com>
2025-09-16 21:10:56 +0700

    fixed error handler logic

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,7 +1,18 @@
-exports.handleError = (res, error) => {
+const createError = (message, status) => {
+  const error = new Error(message);
+  error.status = status;
+  return error;
+};
+
+const handleError = (res, error) => {
   console.error(error);
   res.status(500).json({
     message: "Something Wrong!",
     error: error.message,
   });
+};
+
+module.exports = {
+  handleError,
+  createError, 
 };
