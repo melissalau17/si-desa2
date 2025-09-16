81b9289b0a7171d9126fbd1de6301bea3c7c2f74
melissalau17 <melissalau17300@gmail.com>
2025-09-16 21:38:40 +0700

    edited suratService

--- src/services/suratService.js
+++ src/services/suratService.js
@@ -1,132 +1,59 @@
-const userModel = require("../models/userModel");
-const { hashPassword, verifyPassword } = require("../utils/hashUtils");
-const { createError } = require("../utils/errorHandler");
-const r2Client = require('../r2Config');
-const { DeleteObjectCommand } = require('@aws-sdk/client-s3');
+const suratModel = require("../models/SuratModel");
+const { Buffer } = require("buffer");
 
-// The Base64 conversion function is no longer needed
-// const convertBase64ToBinary = (base64String) => {
-//   const buffer = Buffer.from(base64String, "base64"); 
-//   return buffer;
-// };
+exports.getAllSurat = () => suratModel.findAll();
+exports.getSuratById = (id) => suratModel.findById(id);
+exports.findByNIK = (nik) => suratModel.findByNIK(nik);
+const convertBase64ToBinary = (base64String) => {
+    const buffer = Buffer.from(base64String, "base64"); // Convert Base64 ke Binary
+    return buffer;
+}
 
-exports.getAllUsers = () => userModel.findAll();
+exports.createSurat = (data) => {
+    const {
+        nik,
+        nama,
+        tempat_lahir,
+        tanggal_lahir,
+        jenis_kelamin,
+        agama,
+        alamat,
+        no_hp,
+        email,
+        jenis_surat,
+        tujuan_surat,
+        photo_ktp,
+        photo_kk,
+        foto_usaha,
+        waktu_kematian,
+        gaji_ortu,
+    } = data;
 
-exports.getUserById = (id) => userModel.findById(id);
-
-exports.findByNIK = async (NIK) => {
-  return await userModel.findByNIK(NIK);
+    return suratModel.create({
+        nik,
+        nama,
+        tempat_lahir,
+        tanggal_lahir,
+        jenis_kelamin,
+        agama,
+        alamat,
+        jenis_surat,
+        tujuan_surat,
+        no_hp,
+        email,
+        photo_ktp: photo_ktp ? convertBase64ToBinary(photo_ktp) : null,
+        photo_kk: photo_kk ? convertBase64ToBinary(photo_kk) : null,
+        foto_usaha: foto_usaha ? convertBase64ToBinary(foto_usaha) : null,
+        waktu_kematian,
+        gaji_ortu: gaji_ortu ? convertBase64ToBinary(gaji_ortu) : null,
+    });
 };
 
-exports.createUser = async (data) => {
-  const {
-    nama,
-    username,
-    email,
-    password,
-    photoUrl, // Expecting a URL instead of a Base64 string or buffer
-    NIK,
-    agama,
-    alamat,
-    jenis_kel,
-    no_hp,
-    role,
-  } = data;
+exports.updateSurat = async (id, data) => {
+    const existingSurat = await suratModel.findById(id);
+    if (!existingSurat) return null;
+    const updatedSurat = await suratModel.update(id, data);
+    return updatedSurat;
+}
 
-  const hashedPassword = await hashPassword(password);
-
-  // Simpan user ke dalam database
-  return userModel.create({
-    nama,
-    username,
-    email,
-    password: hashedPassword,
-    photo: photoUrl, // Store the URL directly
-    NIK,
-    agama,
-    alamat,
-    jenis_kel,
-    no_hp,
-    role,
-  });
-};
-
-exports.updateUser = async (id, data, photoUrl) => {
-  const existingUser = await userModel.findById(id);
-  if (!existingUser) return null;
-  
-  const updateData = {
-    nama: data.nama,
-    username: data.username,
-    email: data.email,
-    NIK: data.NIK,
-    agama: data.agama,
-    alamat: data.alamat,
-    jenis_kel: data.jenis_kel,
-    no_hp: data.no_hp,
-    role: data.role,
-  };
-
-  if (data.password) {
-    updateData.password = await hashPassword(data.password);
-  }
-
-  // Handle photo updates
-  if (photoUrl) {
-    // If a new photo is provided, delete the old one from R2
-    if (existingUser.photo) {
-      const oldPhotoKey = existingUser.photo.split('/').slice(4).join('/');
-      await r2Client.send(new DeleteObjectCommand({
-        Bucket: 'sistemdesa',
-        Key: oldPhotoKey
-      }));
-    }
-    updateData.photo = photoUrl;
-  } else if (data.photo === null) {
-    // If the client explicitly requests to delete the photo
-    if (existingUser.photo) {
-      const oldPhotoKey = existingUser.photo.split('/').slice(4).join('/');
-      await r2Client.send(new DeleteObjectCommand({
-        Bucket: 'sistemdesa',
-        Key: oldPhotoKey
-      }));
-    }
-    updateData.photo = null;
-  } else {
-    // Keep the existing photo URL if no new photo is provided
-    updateData.photo = existingUser.photo;
-  }
-
-  return userModel.update(id, updateData);
-};
-
-exports.deleteUser = async (id) => {
-  const user = await userModel.findById(id);
-  if (!user) return null;
-
-  // Delete the associated photo from R2 first
-  if (user.photo) {
-    const photoKey = user.photo.split('/').slice(4).join('/');
-    await r2Client.send(new DeleteObjectCommand({
-      Bucket: 'sistemdesa',
-      Key: photoKey
-    }));
-  }
-
-  // Then, delete the record from the database
-  return userModel.remove(id);
-};
-
-exports.loginUser = async (identifier, password) => {
-  const user = await userModel.findByUsernameOrEmail(identifier);
-  if (!user) {
-    throw createError("Email atau username tidak ditemukan!", 404);
-  }
-
-  const isMatch = await verifyPassword(password, user.password);
-  if (!isMatch) {
-    throw createError("Password salah!", 401);
-  }
-
-  return user;
-};
+exports.deleteSurat = (id) => suratModel.remove(id);
