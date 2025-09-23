f1cca115d7aeb24fe6791b462fa6e8f1d2c82387
melissalau17 <melissalau17300@gmail.com>
2025-09-23 20:53:43 +0800

    added uploads path for photos

--- src/index.js
+++ src/index.js
@@ -79,6 +79,9 @@ app.use((req, res, next) => {
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
+const path = require("path");
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 // Test route
 app.get("/api", (req, res) => {
     res.send("Hello API");
