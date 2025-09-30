1ff8cffcdf4c5e3935f738d9517effeede6c2a6a
melissalau17 <melissalau17300@gmail.com>
2025-10-01 00:53:49 +0700

    added dashboard API to fetch dashboard data

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,7 +1,7 @@
+// dashboardRoutes.js
 const express = require("express");
-const { getDashboard } = require("../controllers/dashboardController");
-const { authMiddleware } = require("../middlewares/authMiddleware");
-
+const getDashboard = require("../controllers/dashboardController").getDashboard; 
+const authMiddleware = require("../middlewares/authMiddleware"); 
 const router = express.Router();
 
 router.get("/", authMiddleware, getDashboard);
