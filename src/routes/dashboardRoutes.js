e80d83a7a5b03dd99d3abc0c707e2d7dfade2532
melissalau17 <melissalau17300@gmail.com>
2025-10-01 13:45:52 +0700

    added endpoint for dashboard data

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,9 +1,8 @@
-// dashboardRoutes.js
 const express = require("express");
 const getDashboard = require("../controllers/dashboardController").getDashboard; 
 const authMiddleware = require("../middlewares/authMiddleware"); 
 const router = express.Router();
 
-router.get("/", authMiddleware, getDashboard);
+router.get("/dashboard", authMiddleware, getDashboard);
 
 module.exports = router;
